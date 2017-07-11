724a52da7b1e47b597ba2bbf52da166a347101d0
Skyler Ruesga <sruesga@fb.com>
2017-07-10 23:07:48 -0700

    fixed slider

--- Tippy/ViewController.swift
+++ Tippy/ViewController.swift
@@ -13,11 +13,23 @@ class ViewController: UIViewController {
     @IBOutlet weak var billField: UITextField!
     @IBOutlet weak var tipLabel: UILabel!
     @IBOutlet weak var totalLabel: UILabel!
-    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var tipControl: UISlider!
+    @IBOutlet weak var tipPercentage: UILabel!
+    
+    let defaults = UserDefaults.standard
+    var tipAmount: Float = 0.15
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        if defaults.object(forKey: "default_tip_percentage") != nil {
+            tipAmount = defaults.float(forKey: "default_tip_percentage")
+            print(tipAmount)
+        }
+        tipControl.setValue(tipAmount, animated: true)
+        tipPercentage.text = String(format: "%.2f%%", tipAmount * 100)
+        defaults.set(tipAmount, forKey: "default_tip_percentage")
+        defaults.synchronize()
     }
 
     override func didReceiveMemoryWarning() {
@@ -29,17 +41,25 @@ class ViewController: UIViewController {
         view.endEditing(true)
     }
     
-    @IBAction func calculateTip(_ sender: AnyObject) {
-        
-        let tipPercentages = [0.18, 0.2, 0.25]
-        
-        let bill = Double(billField.text!) ?? 0
-        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+    @IBAction func calculateTip(_ sender: Any) {
+        let bill = Float(billField.text!) ?? 0
+        let tip = bill * tipAmount
         let total = bill + tip
         
+        tipPercentage.text = String(format: "%.2f%%", tipAmount * 100)
         tipLabel.text = String(format: "$%.2f", tip)
         totalLabel.text = String(format: "$%.2f", total)
     }
 
+    @IBAction func sliderDidMove(_ sender: UISlider) {
+        tipAmount = sender.value
+        tipPercentage.text = String(tipAmount * 100) + "%"
+        calculateTip(self)
+    }
+    
+    @IBAction func setDefault(_ sender: Any) {
+        defaults.set(tipAmount, forKey: "default_tip_percentage")
+        defaults.synchronize()
+    }
 }
 
